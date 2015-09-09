2893c77d7aa66e32cbcda2942e78630b5d7cbcde
Karl Hobley <karlhobley10@gmail.com>
2015-09-09 09:42:40 +0100

    Added api fields for all models

--- demo/models.py
+++ demo/models.py
@@ -59,6 +59,8 @@ class LinkFields(models.Model):
         DocumentChooserPanel('link_document'),
     ]
 
+    api_fields = ('link', )
+
     class Meta:
         abstract = True
 
@@ -81,6 +83,16 @@ class ContactFields(models.Model):
         FieldPanel('country'),
         FieldPanel('post_code'),
     ]
+
+    api_fields = (
+        'telephone',
+        'email',
+        'address_1',
+        'address_2',
+        'city',
+        'country',
+        'post_code',
+    )
 
     class Meta:
         abstract = True
@@ -106,6 +118,12 @@ class CarouselItem(LinkFields):
         MultiFieldPanel(LinkFields.panels, "Link"),
     ]
 
+    api_fields = (
+        'image',
+        'embed_url',
+        'caption',
+    ) + LinkFields.api_fields
+
     class Meta:
         abstract = True
 
@@ -119,6 +137,10 @@ class RelatedLink(LinkFields):
         FieldPanel('title'),
         MultiFieldPanel(LinkFields.panels, "Link"),
     ]
+
+    api_fields = (
+        'title',
+    ) + LinkFields.api_fields
 
     class Meta:
         abstract = True
@@ -170,6 +192,12 @@ class HomePage(Page):
         index.SearchField('body'),
     )
 
+    api_fields = (
+        'body',
+        'carousel_items',
+        'related_links',
+    )
+
     class Meta:
         verbose_name = "Homepage"
 
@@ -201,6 +229,12 @@ class StandardIndexPage(Page):
 
     search_fields = Page.search_fields + (
         index.SearchField('intro'),
+    )
+
+    api_fields = (
+        'intro',
+        'feed_image',
+        'related_links',
     )
 
 StandardIndexPage.content_panels = [
@@ -240,6 +274,14 @@ class StandardPage(Page):
         index.SearchField('body'),
     )
 
+    api_fields = (
+        'intro',
+        'body',
+        'feed_image',
+        'carousel_items',
+        'related_links',
+    )
+
 StandardPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('intro', classname="full"),
@@ -264,6 +306,11 @@ class BlogIndexPage(Page):
 
     search_fields = Page.search_fields + (
         index.SearchField('intro'),
+    )
+
+    api_fields = (
+        'intro',
+        'related_links',
     )
 
     @property
@@ -339,6 +386,15 @@ class BlogPage(Page):
         index.SearchField('body'),
     )
 
+    api_fields = (
+        'body',
+        'tags',
+        'date',
+        'feed_Image',
+        'carousel_items',
+        'related_links',
+    )
+
     @property
     def blog_index(self):
         # Find closest ancestor which is a blog index
@@ -391,6 +447,17 @@ class PersonPage(Page, ContactFields):
         index.SearchField('biography'),
     )
 
+    api_fields = (
+        'first_name',
+        'last_name',
+        'intro',
+        'biography',
+        'image',
+        'feed_image',
+    ) + ContactFields.api_fields + (
+        'related_links',
+    )
+
 PersonPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('first_name'),
@@ -423,6 +490,11 @@ class ContactPage(Page, ContactFields):
         index.SearchField('body'),
     )
 
+    api_fields = (
+        'body',
+        'feed_image',
+    ) + ContactFields.api_fields
+
 ContactPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('body', classname="full"),
@@ -445,6 +517,11 @@ class EventIndexPage(Page):
 
     search_fields = Page.search_fields + (
         index.SearchField('intro'),
+    )
+
+    api_fields = (
+        'intro',
+        'related_links',
     )
 
     @property
@@ -492,6 +569,12 @@ class EventPageSpeaker(Orderable, LinkFields):
         related_name='+'
     )
 
+    api_fields = (
+        'first_name',
+        'last_name',
+        'image',
+    ) + LinkFields.api_fields
+
     @property
     def name_display(self):
         return self.first_name + " " + self.last_name
@@ -531,6 +614,22 @@ class EventPage(Page):
         index.SearchField('get_audience_display'),
         index.SearchField('location'),
         index.SearchField('body'),
+    )
+
+    api_fields = (
+        'date_from',
+        'date_to',
+        'time_from',
+        'time_to',
+        'audience',
+        'location',
+        'cost',
+        'body',
+        'signup_link',
+        'feed_image',
+        'speakers',
+        'carousel_items',
+        'related_links',
     )
 
     @property
